4e27426808574fee0752b2d4c8402408cf68c3fc
Adisa Stewart <adisa.stewart1988@gmail.com>
2016-03-06 15:33:52 -0800

    display result on page

--- js/script.js
+++ js/script.js
@@ -4,4 +4,12 @@ function calculate (height, weight) {
 	var weight = document.getElementById("weight").value
 	var bmindex = weight/(height*height);
 	console.log(bmindex);
+
+
+	// display result on page
+	var newH2 = document.createElement("H2");
+	var result = document.createTextNode("Your Body Mass Index is " + bmindex);
+	newH2.appendChild(result);
+	document.getElementById("result").appendChild(newH2);
 }
+	
