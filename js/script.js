9d6d8ffe61e245677793b93858122183be5e6fc6
Adisa Stewart <adisa.stewart1988@gmail.com>
2016-03-17 00:09:55 -0700

    no longer metric

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 function calculate (height, weight) {
 	var height = document.getElementById("height").value
 	var weight = document.getElementById("weight").value
-	var bmindex = weight/(height*height);
+	var bmindex = (weight/(height*height))*703;
 	console.log(bmindex);
 
 
